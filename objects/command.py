5bb66d39bd8595b482e89317524d919bbeed2d53
Nyo <to@nyo.zz.mu>
2016-12-31 11:51:02 +0100

    L'indentazione

--- objects/command.py
+++ objects/command.py
@@ -4,11 +4,11 @@ class Command:
 		Instantiate a discord bot command
 
 		:param callback: Function object to call when the command is triggered.
-						 must accept a `message` argument, containing the discord Message object
-						 that triggered the command
+						must accept a `message` argument, containing the discord Message object
+						that triggered the command
 		:param syntax: String that contains command's syntax. It's used to check if the message
-					   that should trigger this command has enough parameters.
-					   Eg: "<arg1> <arg2>"
+						that should trigger this command has enough parameters.
+						Eg: "<arg1> <arg2>"
 		"""
 		self.syntax = syntax
 		self.callback = callback
